16a47afae6cb460f650ab31feb0e58f7b15c0910
jess-dam <jess.dam@starlingbank.com>
2022-06-02 00:46:11 +0100

    Allow name setting transaction, display message from smart contract

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import './App.css'
 import "antd/dist/antd.css"
 import { Form, Input, Button } from 'antd'
 import { useState, useEffect } from 'react'
-import { connect, keyStores, Contract } from 'near-api-js'
-import { ACCOUNT_ID } from './config.json'
+import { connect, keyStores, Contract, WalletConnection } from 'near-api-js'
+import config from './config.json'
 
 function App() {
+  const keyStore = new keyStores.BrowserLocalStorageKeyStore()
   const [account, setAccount] = useState({})
   const [contract, setContract]  = useState({})
   const [message, setMessage] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getValidKey = (keys) => {
+    if (keys?.length === 0) {
+      console.log("No signing keys found in local storage!")
+      return null
+    }
+
+  }
 
   const getAccount = async () => {
     const config = {
       networkId: "testnet",
-      keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+      keyStore: keyStore,
       nodeUrl: "https://rpc.testnet.near.org",
       walletUrl: "https://wallet.testnet.near.org",
       helperUrl: "https://helper.testnet.near.org",
@@ -23,7 +33,24 @@ function App() {
     const near = await connect(config)
     const acc = await near.account("bluxue2.testnet")
     setAccount(acc)
+
+    // create wallet connection
+    const wallet = new WalletConnection(near);
+
+    // redirects user to wallet to authorize your dApp
+    // this creates an access key that will be stored in the browser's local storage
+    // access key can then be used to connect to NEAR and sign transactions via keyStore
+    if (keyStore.localStorage.length === 0) signIn(wallet)
   }
+
+  const signIn = (wallet) => {
+    wallet.requestSignIn(
+      "bluxue2.testnet",
+      "Hello World App",
+      "http://localhost:3000",
+      "http://localhost:3000"
+    );
+  };
 
   useEffect(() => {getAccount()}, [])
 
@@ -42,11 +69,12 @@ function App() {
 
 
   const onSubmit = async (value) => {
-    console.log(value)
-    setMessage(value?.name)
-
+    setIsLoading(true)
+    await contract.set_name({ name: value?.name })
     const response = await contract.get_message()
     console.log(response)
+    setMessage(response)
+    setIsLoading(false)
   }
 
   return (
@@ -82,7 +110,14 @@ function App() {
           <Button type="primary" htmlType={onSubmit}>Get My Message</Button>
         </Form>
       </div>
-      <h1 style={{ width: '200px', alignSelf: 'center', padding: '50px' }} >{message}</h1>
+
+      <div style={{ width: '200px', alignSelf: 'center', paddingTop: '50px' }} >
+        {
+          isLoading ?
+            <h1>Loading...</h1>
+            : <h1>{message}</h1>
+        }
+      </div>
     </div>
   );
 }
