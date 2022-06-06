ada86436a71152eb0f07911c092d20f2b5389302
jess-dam <jess.dam@starlingbank.com>
2022-06-07 00:50:57 +0100

    Sign to auth using wallet

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ import config from './config.json'
 
 function App() {
   const keyStore = new keyStores.BrowserLocalStorageKeyStore()
-  const [account, setAccount] = useState({})
+  const [wallet, setWallet] = useState({})
   const [contract, setContract]  = useState({})
   const [message, setMessage] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
-  const getValidKey = (keys) => {
-    if (keys?.length === 0) {
+  const hasValidKey = (keys) => {
+    if (keys === null || keys?.length === 0) {
       console.log("No signing keys found in local storage!")
-      return null
+      return false
     }
 
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+      if (localStorage.key(i).includes(`near-api-js:keystore:${config.CONTRACT_PATH}:testnet`)
+        && localStorage.getItem(localStorage.key(i))) {
+        return true
+      }
+    }
+
+    return false;
   }
 
-  const getAccount = async () => {
-    const config = {
-      networkId: "testnet",
+  const getWallet = async () => {
+    const connectionConfig = {
+      networkId: config.DOMAIN,
       keyStore: keyStore,
       nodeUrl: "https://rpc.testnet.near.org",
       walletUrl: "https://wallet.testnet.near.org",
@@ -30,52 +38,56 @@ function App() {
       explorerUrl: "https://explorer.testnet.near.org",
     }
 
-    const near = await connect(config)
-    const acc = await near.account("bluxue2.testnet")
-    setAccount(acc)
+    const near = await connect(connectionConfig)
 
     // create wallet connection
-    const wallet = new WalletConnection(near);
+    const walletConnection = new WalletConnection(near);
+    setWallet(walletConnection)
 
     // redirects user to wallet to authorize your dApp
     // this creates an access key that will be stored in the browser's local storage
     // access key can then be used to connect to NEAR and sign transactions via keyStore
-    if (keyStore.localStorage.length === 0) signIn(wallet)
+    if (!hasValidKey())
+      signIn(walletConnection)
   }
 
-  const signIn = (wallet) => {
-    wallet.requestSignIn(
-      "bluxue2.testnet",
+  const signIn = async (wallet) => {
+    await wallet.requestSignIn(
+      config.CONTRACT_PATH,
       "Hello World App",
-      "http://localhost:3000",
-      "http://localhost:3000"
+      process.env.CALLBACK_URL,
+      process.env.CALLBACK_URL
     );
   };
 
-  useEffect(() => {getAccount()}, [])
+  const getContract = () => {
+    console.log(wallet)
 
-  useEffect(() => {
     const methodOptions = {
       viewMethods: ['get_message'],
-      changeMethods: ['set_name']
+      changeMethods: ['set_name'],
+      sender: wallet.account()
     }
 
     setContract(new Contract(
-      account,
-      'bluxue2.testnet',
+      wallet?.account(),
+      config.CONTRACT_PATH,
       methodOptions
     ))
-  }, [account])
-
+  }
 
   const onSubmit = async (value) => {
     setIsLoading(true)
+    console.log(contract)
     await contract.set_name({ name: value?.name })
     const response = await contract.get_message()
     console.log(response)
     setMessage(response)
     setIsLoading(false)
   }
+
+  useEffect(() => {getWallet()}, [])
+  useEffect(() => {if (Object.entries(wallet).length > 0) getContract()}, [wallet])
 
   return (
     <div className="App"
@@ -114,7 +126,7 @@ function App() {
       <div style={{ width: '200px', alignSelf: 'center', paddingTop: '50px' }} >
         {
           isLoading ?
-            <h1>Loading...</h1>
+            <h1>Retrieving your message...</h1>
             : <h1>{message}</h1>
         }
       </div>
